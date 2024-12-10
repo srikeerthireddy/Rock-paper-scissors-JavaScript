9d53214522f4889ef5d510c2412619a012e7b1af
srikeerthireddy <srikeerthireddy24@gmail.com>
2024-12-10 13:30:24 +0530

    Rock-paper-Scissors

--- app.js
+++ app.js
@@ -1,47 +1,73 @@
-const computerChoiceDisplay=document.getElementById('computer-choice')
-const userChoiceDisplay=document.getElementById('user-choice')
-const resultDisplay =document.getElementById('result')
-const possibleChoices=document.querySelectorAll('button')
-let userChoice;
-possibleChoices.forEach(possibleChoice=> possibleChoice.addEventListener('click',(e)=>{
-    userChoice=e.target.id;
-    userChoiceDisplay.innerHTML=userChoice
-    generateComputerChoice()
-    getResult()
-}))
-function generateComputerChoice(){
-    const randomNumber=Math.floor(Math.random()*3)+1//or you can use possibleChoices.length
-    // console.log(randomNumber);
-    if(randomNumber==1){
-        computerChoice='rock'
-    }
-    if(randomNumber==2){
-        computerChoice='scissors'
-    }
-    if(randomNumber==3){
-        computerChoice='paper'
-    }
-    computerChoiceDisplay.innerHTML=computerChoice;
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const userChoiceDisplay = document.getElementById('user-choice');
+const resultDisplay = document.getElementById('result');
+const scoresDisplay = document.getElementById('scores');
+const possibleChoices = document.querySelectorAll('button');
+const resetButton = document.getElementById('reset');
 
+let userChoice;
+let computerChoice;
+let result;
+let playerScore = 0;
+let computerScore = 0;
+
+// Event listener for player's choice
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    if (e.target.id !== 'reset') { // Skip the reset button
+        userChoice = e.target.id;
+        userChoiceDisplay.innerHTML = userChoice;
+        generateComputerChoice();
+        getResult();
+    }
+}));
+
+// Generate computer's choice
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3) + 1; // Random number between 1 and 3
+    if (randomNumber === 1) {
+        computerChoice = 'rock';
+    } else if (randomNumber === 2) {
+        computerChoice = 'scissors';
+    } else if (randomNumber === 3) {
+        computerChoice = 'paper';
+    }
+    computerChoiceDisplay.innerHTML = computerChoice;
 }
-function getResult(){
-    if(computerChoice==userChoice){
-        result='Its a draw';
+
+// Determine the result of the round
+function getResult() {
+    if (computerChoice === userChoice) {
+        result = "It's a draw";
+    } else if (
+        (computerChoice === 'rock' && userChoice === 'paper') ||
+        (computerChoice === 'scissors' && userChoice === 'rock') ||
+        (computerChoice === 'paper' && userChoice === 'scissors')
+    ) {
+        result = "You win!";
+        playerScore++;
+    } else {
+        result = "You lose!";
+        computerScore++;
     }
-    if(computerChoice=='rock' &&userChoice=="paper"){
-        result="you win";
-    }
-    if(computerChoice=='rock' &&userChoice=="scissors"){
-        result="you lost";
-    }
-    if(computerChoice=='paper' &&userChoice=="rock"){
-        result="you lose";
-    }
-    if(computerChoice=='scissors' &&userChoice=="rock"){
-        result="you win";
-    }
-    if(computerChoice=='scissors' &&userChoice=="paper"){
-        result="you lose";
-    }
-    resultDisplay.innerHTML=result;
+    resultDisplay.innerHTML = result;
+    updateScores();
 }
+
+// Update scores on the screen
+function updateScores() {
+    scoresDisplay.innerHTML = `Player: ${playerScore} | Computer: ${computerScore}`;
+}
+
+// Reset the game
+resetButton.addEventListener('click', () => {
+    userChoice = '';
+    computerChoice = '';
+    result = '';
+    playerScore = 0;
+    computerScore = 0;
+
+    userChoiceDisplay.innerHTML = '';
+    computerChoiceDisplay.innerHTML = '';
+    resultDisplay.innerHTML = '';
+    updateScores();
+});
